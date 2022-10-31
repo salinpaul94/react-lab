7f8ff8b7da59a49c75ffb86a732e731ee1571d82
salinpaul94 <salinpv@hotmail.com>
2022-10-31 16:23:18 +0530

    connecting to json server and fetching data and console log data

--- client/src/components/expense-tracker.tsx
+++ client/src/components/expense-tracker.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { getAllExpenseItems } from "../services/expense";
+
 const ExpenseTracker = () => {
+
+    useEffect(() => {
+
+        const getAllExpenseItemsInvoker = async () => {
+            const responseData = await getAllExpenseItems();
+            console.log(responseData);
+        }
+
+        getAllExpenseItemsInvoker();
+        
+    }, []);
 
     return (
         <div>
---
+++ client/src/models/expense.ts
@@ -0,0 +1,10 @@
+
+interface IExpenseItem {
+    payeeName: string,
+    expenseDescription: string,
+    price: number,
+    date: Date,
+    id: number
+}
+
+export default IExpenseItem;
